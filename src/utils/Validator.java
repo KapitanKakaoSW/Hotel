db7705f3fc1f885b5814a9e19b7f67f050b93831
KapitanKakao <kapitan.kakao52375@gmail.com>
2021-12-09 04:50:58 +0200

    Add validator for children

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -53,4 +53,23 @@ public class Validator {
         }
         return operation;
     }
+
+    public static int validateChildrenInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите количество!: ");
+        }
+        int quantity = scanner.nextInt();
+        while (quantity < 0) {
+            System.out.println("Неверное значение! Введите количество: ");
+            while (!scanner.hasNextInt()) {
+                String str = scanner.next().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.println("Введите количество!: ");
+            }
+            quantity = scanner.nextInt();
+        }
+        return quantity;
+    }
 }
